c5f238ae9e55cac6650dd7a0e9c081842c3ebe24
elie <elievatine@live.fr>
2018-09-05 13:06:07 +0200

    first commit

---
+++ src/components/resume/resumeData.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+
+export const singlePage = {}
+export const doublePage = {
+ education: {
+    name: 'education',
+    details: [{
+        title: 'Epitech',
+        location: 'Paris',
+        duration: 'Sept. 2017 - Sept. 2022 (expected)',
+        subTitle: 'Master\'s degree in Developer'
+      },
+      {
+        title: 'Carnot High School',
+        location: 'Paris',
+        duration: 'Sept. 2012 - Jun. 2015',
+        subTitle: 'Baccalaureat in Economic Science and Social'
+      }
+    ]
+  },
+  skills: {
+    name: 'skills/abilities',
+    items: ['Python', 'JavaScript', 'C/C++', 'Java', 'HTML/CSS', 'GraphQL', 'Git', 'Vue/React',
+      'Devops', 'AWS/GCP', 'Docker', 'PWA', 'Serverless', 'Deep Learning'
+    ]
+  },
+ 
+}
